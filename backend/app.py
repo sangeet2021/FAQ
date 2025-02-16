50acf5bf1fc526f774ce7d8b2a4bf708d7edeb85
sangeet2021 <stiletdurden252@gmail.com>
2025-02-16 23:37:21 +0545

    backend

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,16 @@
 from flask import Flask, request, jsonify
 from firebase_admin import credentials, firestore
 import requests
+import os
+from dotenv import load_dotenv
+import deepseek
 
 app = Flask(__name__)
 
 #Firebase
 
+load_dotenv();
+
 # Deepseek API endpoint and key
 DEEPSEEK_API_URL = "https://api.deepseek.com/v1/chat/completions"
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API")
